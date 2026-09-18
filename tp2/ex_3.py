94d8f175c088b68f1ed768dd622f61996032aedb
Romain FLICHY <romain.flichy@reseau.eseo.fr>
2026-09-18 13:56:55 +0200

    exo3,tp2

--- tp2/ex_3.py
+++ tp2/ex_3.py
@@ -2,9 +2,18 @@ import random
 n=int(input("Combien de valeur? (entre 2 et 100)\n"))
 liste=[random.randint(0,500) for i in range(n)]
 verif=True
+run=True
 
-while verif:
+while verif and run:
     for elem in liste:
         for i in range(len(liste)):
             if elem==i:
                 verif=False
+    run=False
+
+if not verif:
+    print("Valeurs similaire détectée")
+else:
+    print("Aucune valeurs similaire détectée")
+
+print(liste)
